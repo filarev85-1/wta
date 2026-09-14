410cdf4acef94d1badbd2d63facccb25b9f16fc3
전정제 CX운영실(NOL) <jeongje.jeon@yanolja.group>
2026-09-14 11:33:51 +0900

    캐시 초기화 및 PWA 설치 업데이트 v2

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,23 @@
+const CACHE_NAME = 'wta-cache-v2';
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  return self.clients.claim();
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  // 웹 앱 네트워크 요청 중계
+  // 네트워크 기본 요청 처리
 });
